90d94abee26b52731dfaefe9107f2b61a093f78c
Sergey Pigach <p.i.g.a.c.h@gmail.com>
2022-08-19 13:20:40 -0400

    Adding dist

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.VUE_APP_NODE_ENV === "production" ? "/CORE.Cortex.Benchmark/" : "/"
 })
